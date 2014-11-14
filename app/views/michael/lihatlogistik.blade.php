f7e1a1b89dac134c5e2721cee78115d396e4bd64
michaelaw320 <michaelaw320@gmail.com>
2014-11-14 21:39:48 +0700

    Template

---
+++ app/views/michael/lihatlogistik.blade.php
@@ -0,0 +1,17 @@
+@extends("layout")
+@section("css")
+	<style type="text/css">
+		/* Fill in your css */
+	</style>
+@stop
+@section("content")
+	<div class="starter-template">
+		<!-- Fill in your content -->
+		<button class="submitbutton" type="submit">Login</button>
+	</div>
+@stop
+@section("javascript")
+	<script type="text/javascript">
+		/* Fill in your javascript */
+	</script>
+@stop
---
+++ app/views/michael/tambahlogistik.blade.php
@@ -0,0 +1,21 @@
+@extends("layout")
+@section("css")
+	<style type="text/css">
+		/* Fill in your css */
+	</style>
+@stop
+@section("content")
+	<div class="starter-template">
+		<h1> Tambah data logistik </h1>
+		<br>
+		<form class="form"> 
+			Jenis Logistik: 
+		</form>
+		<button class="submitbutton" type="submit">Login</button>
+	</div>
+@stop
+@section("javascript")
+	<script type="text/javascript">
+		/* Fill in your javascript */
+	</script>
+@stop
