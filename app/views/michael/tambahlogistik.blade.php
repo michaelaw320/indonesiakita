fc5eee78bc93fa7069f88f33c04bea62bc0e9913
Andarias <asilvanus5@gmail.com>
2014-11-16 23:11:43 +0700

    16 Nov 23:11

--- app/views/michael/tambahlogistik.blade.php
+++ app/views/michael/tambahlogistik.blade.php
@@ -2,20 +2,46 @@
 @section("css")
 	<style type="text/css">
 		/* Fill in your css */
+		#btnUpdateData {
+			width: 100%;
+		}
 	</style>
 @stop
 @section("content")
 	<div class="starter-template">
-		<h1> Tambah data logistik </h1>
-		<br>
-		<form class="form"> 
-			Jenis Logistik: <select class="form-control">
-						  <option>Beras</option>
-						  <option>Air</option>
-						  <option>Other</option>
-						  <!-- TODO nanti kalau dipilih other dia bisa input yg laen, tolong dilengkapin -->
+		<table class="table table-condensed" style="display: inline-block;">
+			<tr>
+				<td colspan="2">
+					<img src="{{ URL::asset('img/cover_tambah_logistik.jpg') }}">
+				</td>
+			</tr>
+			<tr>
+				<td width="400">
+					<form class="form"> 
+						<h3>Jenis Logistik:</h3> 
+						<select class="form-control">
+ 						<option>Makanan</option>
+						<option>Air</option>
+						<option>Pakaian</option>
+						<option>Kendaraan</option>
+						<option>Peralatan Tukang</option>
+						<option>Other</option>
+						<!-- TODO nanti kalau dipilih other dia bisa input yg laen, tolong dilengkapin -->
 						</select>
-		</form>
+					</form>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<h5> <input type="text" value="Jumlah" > </h5>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<center><button id="btnUpdateData" class="btn btn-lg btn-success" type="submit">Update Data</button></center>
+				</td>
+			</tr>
+		</table>
 	</div>
 @stop
 @section("javascript")
